a21b1fdd3926e4ce5d3b64c2e3c9a16b593ce67c
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2022-10-25 17:45:43 -0400

    Update package version

--- src/components/ProvidersSelector.svelte
+++ src/components/ProvidersSelector.svelte
@@ -16,28 +16,22 @@
     const goto = (provider: ProviderId) => {
         $vfs.setProvider(provider)
     }
-  </script>
+</script>
   
-  <div class="overflow-x-auto">
+<div class="overflow-x-auto">
     <table class="table w-full">
-      <!-- head -->
-      <thead>
-        <tr>
-          <th></th>
-          <th>Name</th>
-          <th>Modified at</th>
-          <th>Size</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each services as service}
-          <tr class="hover" on:dblclick={() => goto(service)}>
-            <td></td>
-            <td>{service.id}</td>
-            <td>Cy Ganderton</td>
-            <td>Blue</td>
-          </tr>
-        {/each}
-      </tbody>
+        <!-- head -->
+        <thead>
+            <tr>
+                <th>Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each services as service}
+                <tr class="hover" on:dblclick={() => goto(service)}>
+                    <td>{service.id}</td>
+                </tr>
+            {/each}
+        </tbody>
     </table>
-  </div>
+</div>
